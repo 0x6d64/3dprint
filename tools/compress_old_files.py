6f67432bcbe286862cc7f6692054d1e8dad75235
Martin Demling <0x6d64@martin-demling.de>
2023-04-09 16:55:25 +0300

    add type hint, tighten code

--- tools/compress_old_files.py
+++ tools/compress_old_files.py
@@ -9,7 +9,18 @@ import stat
 import zipfile
 from collections import namedtuple
 from dataclasses import dataclass
-from typing import List
+from typing import List, Union
+
+"""
+further improvements:
+- complete implementation that compresses old gcode files
+"""
+
+
+_log = logging.getLogger(__name__)
+_log.addHandler(logging.StreamHandler())
+_log.setLevel(logging.DEBUG)
+
 
 FolderDate = namedtuple("FolderDate", ["year", "month"])
 
@@ -36,18 +47,13 @@ class CompressionStats:
         )
 
 
-_log = logging.getLogger(__name__)
-_log.addHandler(logging.StreamHandler())
-_log.setLevel(logging.DEBUG)
-
-
-def remove_readonly(func, path, excinfo):
+def remove_readonly(func, path, excinfo_ignored):
     # Using os.chmod with stat.S_IWRITE to allow write permissions
     os.chmod(path, stat.S_IWRITE)
     func(path)
 
 
-def sizeof_fmt(num, suffix="B"):
+def sizeof_fmt(num: Union[int, float], suffix="B") -> str:
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
         if abs(num) < 1024.0:
             return f"{num:3.1f}{unit}{suffix}"
@@ -57,17 +63,13 @@ def sizeof_fmt(num, suffix="B"):
 
 def get_parsed_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-
     parser.add_argument("--basedir", help="basedir for search", required=True)
-
     return parser.parse_args()
 
 
 def _get_old_monthly_folders(
     basedir: pathlib.Path, min_age_months=1
 ) -> List[pathlib.Path]:
-    current_year = datetime.datetime.now().year
-    current_month = datetime.datetime.now().month
     retval = []
 
     if not basedir.exists():
@@ -81,14 +83,16 @@ def _get_old_monthly_folders(
                 if not date:
                     continue
                 age_in_months = month_difference(
-                    current_year, current_month, date.year, date.month
+                    datetime.datetime.now().year,
+                    datetime.datetime.now().month,
+                    date.year,
+                    date.month,
                 )
                 if age_in_months >= min_age_months:
                     _log.info(f"found candidate: {str(item)}")
                     retval.append(item)
                 else:
                     _log.debug(f"item too young: {str(item)}")
-
     return retval
 
 
@@ -106,7 +110,7 @@ def month_difference(year_a: int, month_a: int, year_b: int, month_b: int) -> in
     return diff
 
 
-def _get_date_from_path(path: pathlib.Path):
+def _get_date_from_path(path: pathlib.Path) -> Union[FolderDate, None]:
     folder_pattern = "(?P<year>[0-9]{4})-(?P<month>[0-9]{1,2})"
     match = re.match(folder_pattern, str(path.stem))
 
@@ -119,7 +123,9 @@ def _get_date_from_path(path: pathlib.Path):
     return retval
 
 
-def discover_old_input_directories(basedir: pathlib.Path, min_age_months=2):
+def discover_old_input_directories(
+    basedir: pathlib.Path, min_age_months=2
+) -> list[pathlib.Path]:
     old_month_folder = _get_old_monthly_folders(
         basedir=basedir, min_age_months=min_age_months
     )
@@ -136,22 +142,25 @@ def discover_old_input_directories(basedir: pathlib.Path, min_age_months=2):
     return old_input_folders
 
 
-def compress_and_delete(input_list: List[pathlib.Path]):
+def get_folder_size(folder: pathlib.Path) -> int:
+    size = sum(f.stat().st_size for f in folder.glob("**/*") if f.is_file())
+    return size
+
+
+def compress_and_delete_folder(input_list: List[pathlib.Path]) -> CompressionStats:
     size_uncompressed, size_compressed = 0.0, 0.0
     for item in input_list:
-        arch_path = shutil.make_archive(
+        archive_path = shutil.make_archive(
             str(item), format="zip", root_dir=str(item), logger=_log
         )
-        # archive_name = os.path.
-        # shutil.make_archive()
-        with zipfile.ZipFile(arch_path) as arch:
-            arch_check_error = arch.testzip()
-            if arch_check_error:
-                raise RuntimeError(f"bad zipfile {arch_path}")
+        with zipfile.ZipFile(archive_path) as arch:
+            archive_check_has_error = arch.testzip()
+            if archive_check_has_error:
+                raise RuntimeError(f"bad zipfile {archive_path}")
             else:
-                _log.debug(f"success: {str(arch_path)}")
+                _log.debug(f"success: {str(archive_path)}")
         item_uncompressed = get_folder_size(item)
-        item_compressed = pathlib.Path(arch_path).stat().st_size
+        item_compressed = pathlib.Path(archive_path).stat().st_size
         _log.debug(
             f"uncompressed: {sizeof_fmt(item_uncompressed)}, compressed: {sizeof_fmt(item_compressed)}"
         )
@@ -166,16 +175,14 @@ def compress_and_delete(input_list: List[pathlib.Path]):
     )
 
 
-def get_folder_size(folder: pathlib.Path):
-    size = sum(f.stat().st_size for f in folder.glob("**/*") if f.is_file())
-    return size
-
-
 def run_main(args: argparse.Namespace):
     basedir = pathlib.Path(args.basedir)
 
     input_dirs_to_compress = discover_old_input_directories(basedir)
-    stats = compress_and_delete(input_dirs_to_compress)
+    stats_input_dirs = compress_and_delete_folder(input_dirs_to_compress)
+
+    stats = stats_input_dirs
+
     _log.info(
         f"found {stats.count} items, compressed to {sizeof_fmt(stats.compressed)}, "
         f"ratio: {100 * stats.ratio if stats.ratio else 0.0:2.3}% ({sizeof_fmt(stats.saved)} saved)"
