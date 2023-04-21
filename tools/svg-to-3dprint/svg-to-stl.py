9668465fd694e5b5fddd016bb90066a0234f5930
Martin Demling <0x6d64@martin-demling.de>
2023-04-21 18:18:07 +0300

    start with solidpython2

--- tools/svg-to-3dprint/svg-to-stl.py
+++ tools/svg-to-3dprint/svg-to-stl.py
@@ -1,9 +1,11 @@
 """Helper to create a STL file from an image."""
 import argparse
+import solid2 as sd  # see https://github.com/jeff-dh/SolidPython#version-2-x-x
+
 
 def run_main():
     pass
 
+
 if __name__ == '__main__':
     pass
-
